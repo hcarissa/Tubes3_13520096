1aa8f215533810d9af1917ec924908774d5e2804
adeliaaaa <monicadelia88@gmail.com>
2022-04-21 22:51:53 +0700

    logo and appName FE

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from "react";
+import { StyleSheet, View, Dimensions } from "react-native";
+import getWindowDimensions from "./components/getWindowDimensions";
+import logoDNA from './dna.png';
+
 
 function App() {
   return (
-    <div className="App">
-      Hello, World!
+    <div className='App'>
+      <header data-role="Header" className='header'>
+        <div className='container1'>
+          <img src={logoDNA} className='Logo' />
+          <span className='App-Name'> search.dna </span>
+        </div>
+      </header>
+
+      <div data-role="body" className='container'>
+      </div>
     </div>
   );
 }
