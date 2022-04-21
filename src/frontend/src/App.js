691fd9d0ed8689fb0eca9ad76987b65afd3e6c1c
hcarissa <13520164@std.stei.itb.ac.id>
2022-04-21 23:24:05 +0700

    change tab logo and title

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react";
-import { StyleSheet, View, Dimensions } from "react-native";
+// import { StyleSheet, View, Dimensions } from "react-native";
 import getWindowDimensions from "./components/getWindowDimensions";
 import logoDNA from './dna.png';
 
