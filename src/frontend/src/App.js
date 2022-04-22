098dc14057fcdb483cb4974f203983d5a98dd0db
adeliaaaa <monicadelia88@gmail.com>
2022-04-22 23:52:45 +0700

    add fitur page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { Component } from "react";
-// import { StyleSheet, View, Dimensions } from "react-native";
-import getWindowDimensions from "./components/getWindowDimensions";
 import logoDNA from './dna.png';
-
 
 function App() {
   return (
@@ -26,7 +22,7 @@ function App() {
             <h1 className='text-FiturKami'>FITUR KAMI</h1>
             <div className='box1'>
               <h1 className='text-FiturFitur'>Riwayat Prediksi</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices sapien nec ultricies posuere. Nulla facilisi. Vivamus luctus, leo vel consequat ullamcorper, justo ex sodales urna, vel porttitor sem nulla quis ex. Proin vestibulum lectus eu lorem congue consequat. Proin feugiat dui orci, eget varius ligula viverra vitae. Proin ac nulla pulvinar, facilisis ante sed, scelerisque mi. Phasellus eget pellentesque quam. Integer ultrices sapien lorem, eget porta leo porta nec.</p>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent ultrices sapien nec ultricies posuere.</p>
             </div>
             <div className='box1'>
               <h1 className='text-FiturFitur'>Tambah Penyakit</h1>
